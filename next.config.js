b88a9a306c30c958db3877853ec5b550cccef1bf
Alex <alex@Alexs-MBP.fritz.box>
2024-03-19 19:48:45 +0100

    fix-2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/out',
-    assetPrefix: '/out'
+    basePath: '/learnapp/out',
+    assetPrefix: '/learnapp/out'
 }
 
 module.exports = nextConfig
